15edb1707d08fe50a67306d1a52e401830439f06
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2022-04-06 12:11:21 +0530

    Update node.js

--- node.js
+++ node.js
@@ -12,4 +12,3 @@ var uniqueBuildplaneImages = combined.filter((v,index,a) => console.log(a));
 
 // var uniqueBuildplaneImages = combined.filter((v,index,a)=>a.findIndex(v2=>(v2.imageId===v.imageId))===index);
 fs.writeFileSync('newBuildPlaneImages.json', JSON.stringify(uniqueBuildplaneImages, null, 2), {flag: 'w'});
-console.log(uniqueBuildplaneImages);
