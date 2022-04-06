cbe0973b46e0c0642104dc83ecba0a6c9c2ffedc
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2022-04-06 13:26:17 +0530

    Update node.js

--- node.js
+++ node.js
@@ -8,19 +8,11 @@ var combined = json1.concat(json2);
 combined = combined.concat(json3);
 var mySet = new Set();
 let uniqueBuildPlaneImages = [];
-
-// console.log(combined);
-
 combined.map (eachItem => {
   if (!mySet.has(eachItem.imageId)) {
     mySet.add(eachItem.imageId);
     uniqueBuildPlaneImages.push(eachItem);
   }
 });
-
-console.log(uniqueBuildPlaneImages);
-// var uniqueBuildplaneImages = combined.filter((v,index,a) =
-                                             
-
-// var uniqueBuildplaneImages = combined.filter((v,index,a)=>a.findIndex(v2=>(v2.imageId===v.imageId))===index);
-// fs.writeFileSync('newBuildPlaneImages.json', JSON.stringify(uniqueBuildplaneImages, null, 2), {flag: 'w'});
+console.log(uniqueBuildPlaneImages);   
+fs.writeFileSync('newBuildPlaneImages.json', JSON.stringify(uniqueBuildplaneImages, null, 2), {flag: 'w'});
