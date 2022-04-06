fcdd67760da7ba18beec167e473b9366e0dff764
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2022-04-06 12:28:47 +0530

    Update node.js

--- node.js
+++ node.js
@@ -6,9 +6,19 @@ var json3 = require('./buildplanegcpimages.json');
 
 var combined = json1.concat(json2);
 combined = combined.concat(json3);
+var myset = new Set();
+let uniqueBuildPlaneImages = [];
 
 console.log(combined);
 
+combined.map (eachItem => {
+  if (!mySet.has(eachItem.imageId)) {
+    mySet.add(imageId);
+    uniqueBuildPlaneImages.push(eachItem);
+  }
+});
+
+console.log(uniqueBuildPlaneImages);
 // var uniqueBuildplaneImages = combined.filter((v,index,a) =
                                              
 
